bdf37824f6c6d65eaf776ccfe89a4045f46a6536
Lucas Nicolau <nicolau.lucas04@gmail.com>
2025-07-15 19:30:50 -0300

    Update mlab0-ex03-soma_vetor.c

--- Mini-Lab - Módulo 0 - Programação com Threads em Problemas Clássicos/Mini-Lab - Módulo 0 - Programação com Threads em Problemas Clássicos/Soma vetor/mlab0-ex03-soma_vetor.c
+++ Mini-Lab - Módulo 0 - Programação com Threads em Problemas Clássicos/Mini-Lab - Módulo 0 - Programação com Threads em Problemas Clássicos/Soma vetor/mlab0-ex03-soma_vetor.c
@@ -2,68 +2,68 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-typedef struct{
+typedef struct {
     int* vetor;
     int inicio;
     int fim;
     int* resultado;
 } ThreadArgs;
 
-void* thread_func(void* arg){
+void* thread_func(void* arg) {
     ThreadArgs* args = (ThreadArgs*)arg;
-    
+
     int soma = 0;
-    for (int i = args->inicio; i < args->fim; i++){
+    for (int i = args->inicio; i < args->fim; i++) {
         soma += args->vetor[i];
     }
+
     *(args->resultado) = soma;
-    pthread_exit(NULL);
+    return NULL;
 }
 
-int main(int argc, char* argv[]){
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Uso: %s <tamanho_vetor> <numero_threads>\n", argv[0]);
+        return 1;
+    }
+
     int tamanho = atoi(argv[1]);
     int num_threads = atoi(argv[2]);
 
     printf("Executando soma_vetor...\n");
+
     int* vetor = malloc(tamanho * sizeof(int));
-    for (int i = 0; i < tamanho; i++){
+    for (int i = 0; i < tamanho; i++) {
         vetor[i] = rand() % 100;
     }
 
     printf("Vetor gerado:\n");
-    for (int i = 0; i < tamanho; i++){
+    for (int i = 0; i < tamanho; i++) {
         printf("%d ", vetor[i]);
     }
     printf("\n");
 
-    // Determinar o tamanho de cada parte do vetor para cada thread
     int parte = tamanho / num_threads;
 
-    // Criar as threads e armazenar os resultados parciais
     pthread_t* threads = malloc(num_threads * sizeof(pthread_t));
     ThreadArgs* args = malloc(num_threads * sizeof(ThreadArgs));
     int* resultadosParciais = malloc(num_threads * sizeof(int));
 
-    // Criar as threads
-    for (int i = 0; i < num_threads; i++){
+    for (int i = 0; i < num_threads; i++) {
         args[i].vetor = vetor;
         args[i].inicio = i * parte;
-        if (i == num_threads - 1) {
-            args[i].fim = tamanho;
-        } 
-        else {
-            args[i].fim = (i + 1) * parte;
-        }
+        args[i].fim = (i == num_threads - 1) ? tamanho : (i + 1) * parte;
         args[i].resultado = &resultadosParciais[i];
+
         pthread_create(&threads[i], NULL, thread_func, &args[i]);
     }
 
-    for (int i = 0; i < num_threads; i++){
+    for (int i = 0; i < num_threads; i++) {
         pthread_join(threads[i], NULL);
     }
 
     int soma_total = 0;
-    for (int i = 0; i < num_threads; i++){
+    for (int i = 0; i < num_threads; i++) {
         soma_total += resultadosParciais[i];
     }
 
